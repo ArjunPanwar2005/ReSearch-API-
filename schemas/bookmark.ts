f58ec800cb5597996777d1f95f84a7800b75e41a
ARJUN PANWAR <97743298+ArjunPanwar2005@users.noreply.github.com>
2022-03-01 15:06:59 +0530

    Add files via upload

---
+++ schemas/bookmark.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+const bookmarkSchema = new mongoose.Schema({
+  bookmarkid: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  links: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true
+      },
+      contentid: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+export default mongoose.model("bookmark", bookmarkSchema)
